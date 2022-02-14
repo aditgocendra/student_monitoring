f510c7402d9c752b398178d65ea9a22a2c2edce9
aditgocendra <gocendra123@gmail.com>
2022-02-15 02:09:31 +0800

    add redirect after sign in

--- app/src/main/java/com/ark/studentmonitoring/MainActivity.java
+++ app/src/main/java/com/ark/studentmonitoring/MainActivity.java
@@ -1,8 +1,11 @@
 package com.ark.studentmonitoring;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import com.ark.studentmonitoring.View.Auth.SignIn;
+import com.ark.studentmonitoring.View.User.HomeApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (user != null){
+            Utility.updateUI(MainActivity.this, HomeApp.class);
+        }else {
+            Utility.updateUI(MainActivity.this, SignIn.class);
+        }
+        finish();
     }
 }
