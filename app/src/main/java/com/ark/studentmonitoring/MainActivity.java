2629c6366737c36fe1327b03b127957228064d85
aditgocendra <gocendra123@gmail.com>
2022-02-15 23:36:10 +0800

    add handler and set window flag

--- app/src/main/java/com/ark/studentmonitoring/MainActivity.java
+++ app/src/main/java/com/ark/studentmonitoring/MainActivity.java
@@ -1,7 +1,9 @@
 package com.ark.studentmonitoring;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
 import com.ark.studentmonitoring.View.Auth.SignIn;
 import com.ark.studentmonitoring.View.User.HomeApp;
 import com.google.firebase.auth.FirebaseAuth;
@@ -13,14 +15,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Utility.checkWindowSetFlag(this);
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        Handler handler = new Handler();
 
-        if (user != null){
-            Utility.updateUI(MainActivity.this, HomeApp.class);
-        }else {
-            Utility.updateUI(MainActivity.this, SignIn.class);
-        }
-        finish();
+        handler.postDelayed(() -> {
+            if (user != null){
+                Utility.uidCurrentUser = user.getUid();
+                Utility.updateUI(MainActivity.this, HomeApp.class);
+            }else {
+                Utility.updateUI(MainActivity.this, SignIn.class);
+            }
+            finish();
+        }, 1000);
+
     }
 }
