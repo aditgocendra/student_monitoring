6fbde0a71989c6bc1a0def0711e9607b7dc4bccd
aditgocendra <gocendra123@gmail.com>
2022-02-15 23:36:58 +0800

    add checker connection

--- app/src/main/java/com/ark/studentmonitoring/Utility.java
+++ app/src/main/java/com/ark/studentmonitoring/Utility.java
@@ -1,16 +1,30 @@
 package com.ark.studentmonitoring;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
+import android.net.NetworkRequest;
 import android.os.Build;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.AppCompatButton;
+
 public class Utility {
+
+    public static String uidCurrentUser;
+    public static String roleCurrentUser;
+    public static String usernameCurrentUser;
 
     public static void updateUI(Context context, Class to){
         Intent intent = new Intent(context, to);
@@ -30,12 +44,13 @@ public class Utility {
             setWindowFlag(activity, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, true);
         }
         if (Build.VERSION.SDK_INT >= 19) {
-            activity.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+            activity.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         }
         //make fully Android Transparent Status bar
         if (Build.VERSION.SDK_INT >= 21) {
             setWindowFlag(activity, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false);
             activity.getWindow().setStatusBarColor(Color.TRANSPARENT);
+
         }
     }
 
@@ -65,5 +80,31 @@ public class Utility {
 
         return validate;
     }
+
+    public static boolean isConnected = false;
+    public static boolean checkConnection(Context mContext){
+
+        ConnectivityManager connectivityManager = (ConnectivityManager) mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            connectivityManager.registerDefaultNetworkCallback(new ConnectivityManager.NetworkCallback(){
+                @Override
+                public void onAvailable(@NonNull Network network) {
+                    super.onAvailable(network);
+                    isConnected = true;
+                }
+                @Override
+                public void onLost(@NonNull Network network) {
+                    super.onLost(network);
+                    isConnected = false;
+                }
+            });
+        }else {
+            NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
+            isConnected = activeNetwork != null &&
+                    activeNetwork.isConnectedOrConnecting();
+        }
+        return isConnected;
+    }
+
 
 }
