594e34193e9eca82ff3a3a7321abc5b2445c918d
aditgocendra <gocendra123@gmail.com>
2022-02-17 23:29:28 +0800

    CRUD detail teacher and parent

--- app/src/main/java/com/ark/studentmonitoring/View/User/Profile.java
+++ app/src/main/java/com/ark/studentmonitoring/View/User/Profile.java
@@ -6,6 +6,9 @@ import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.view.View;
+
+import com.ark.studentmonitoring.Model.ModelParent;
+import com.ark.studentmonitoring.Model.ModelTeacher;
 import com.ark.studentmonitoring.Model.ModelUser;
 import com.ark.studentmonitoring.NetworkChangeListener;
 import com.ark.studentmonitoring.R;
@@ -17,6 +20,7 @@ import com.ark.studentmonitoring.databinding.ActivityProfileBinding;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -34,7 +38,7 @@ public class Profile extends AppCompatActivity {
 
         setLayoutRole();
         listenerClick();
-        setDataUser();
+
     }
 
 
@@ -52,6 +56,8 @@ public class Profile extends AppCompatActivity {
     }
 
     private void setLayoutRole() {
+        setDataUser();
+
         if (Utility.roleCurrentUser.equals("admin")){
             binding.administratorBtn.setVisibility(View.VISIBLE);
         }
@@ -60,11 +66,13 @@ public class Profile extends AppCompatActivity {
             binding.teacherBtn.setVisibility(View.VISIBLE);
             binding.cardFeatureRollTeacher.setVisibility(View.VISIBLE);
             binding.profileImg.setImageResource(R.drawable.default_teacher_photo);
+            setDataTeacher();
         }
 
         if (Utility.roleCurrentUser.equals("parent")){
             binding.cardFeatureRollParent.setVisibility(View.VISIBLE);
             binding.profileImg.setImageResource(R.drawable.default_parent_photo);
+            setDataChildParent();
         }
     }
 
@@ -92,6 +100,7 @@ public class Profile extends AppCompatActivity {
             String email = binding.emailEditTi.getText().toString();
             String username = binding.usernameEditTi.getText().toString();
             String phone_number = binding.phoneNumberEditTi.getText().toString();
+            String address = binding.streetProfileTi.getText().toString();
 
             if (email.isEmpty()){
                 binding.emailEditTi.setError("Email tidak boleh kosong");
@@ -99,28 +108,118 @@ public class Profile extends AppCompatActivity {
                 binding.usernameEditTi.setError("Username tidak boleh kosong");
             }else if (phone_number.isEmpty()){
                 binding.phoneNumberEditTi.setError("Nomor telepon tidak boleh kosong");
+            }else if (address.isEmpty()){
+                binding.streetProfileTi.setError("Alamat tidak boleh kosong");
             }else {
-                saveDataProfile(email, username, phone_number);
+                binding.progressCircular.setVisibility(View.VISIBLE);
+                binding.progressCircular.setEnabled(false);
+                saveDataProfile(email, username, phone_number, address);
+            }
+        });
+
+        binding.editDataTeacherBtn.setOnClickListener(view -> {
+            String full_name = binding.fullnameTeacherTi.getText().toString();
+            String nip = binding.nipGuruTi.getText().toString();
+
+            if (full_name.isEmpty()){
+                binding.fullnameTeacherTi.setError("Nama lengkap harus diisi");
+            }else if (nip.isEmpty()){
+                binding.nipGuruTi.setError("Nip harus diisi");
+            }else {
+                if (Utility.roleCurrentUser.equals("teacher")){
+                    binding.progressCircular.setVisibility(View.VISIBLE);
+                    binding.progressCircular.setEnabled(false);
+                    saveDataTeacher(full_name, nip);
+                }else {
+                    Utility.toastLS(Profile.this, "Anda bukan guru");
+                }
+            }
+        });
+
+        binding.editDataParentChild.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name_child = binding.nameChildTi.getText().toString();
+                String nisn = binding.nisnEditTi.getText().toString();
+
+                if (name_child.isEmpty()){
+                    binding.nameChildTi.setError("Nama anak tidak boleh kosong");
+                }else if (nisn.isEmpty()){
+                    binding.nisnEditTi.setError("nisn tidak boleh kosong");
+                }else {
+                    if (Utility.roleCurrentUser.equals("parent")){
+                        binding.progressCircular.setVisibility(View.VISIBLE);
+                        binding.progressCircular.setEnabled(false);
+                        saveDataParent(name_child, nisn);
+                    }else {
+                        Utility.toastLS(Profile.this, "Anda bukan wali murid");
+                    }
+                }
             }
         });
     }
 
-    private void saveDataProfile(String email, String username, String phone_number) {
+    private void saveDataProfile(String email, String username, String phone_number, String address) {
         DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
 
         ModelUser modelUser = new ModelUser(
                 username,
                 email,
                 Utility.roleCurrentUser,
-                phone_number
+                phone_number,
+                address
         );
 
-        reference.child("users").child(Utility.uidCurrentUser).setValue(modelUser).addOnCompleteListener(new OnCompleteListener<Void>() {
+        reference.child("users").child(Utility.uidCurrentUser).setValue(modelUser).addOnCompleteListener(task -> {
+            if (task.isSuccessful()){
+                binding.progressCircular.setVisibility(View.GONE);
+                binding.progressCircular.setEnabled(true);
+                Utility.toastLS(Profile.this, "Berhasil mengubah profile");
+            }else {
+                binding.progressCircular.setVisibility(View.GONE);
+                binding.progressCircular.setEnabled(true);
+                Utility.toastLS(Profile.this, task.getException().getMessage());
+            }
+        });
+    }
+
+    private void saveDataTeacher(String full_name, String nip){
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+
+        ModelTeacher modelTeacher = new ModelTeacher(
+                full_name,
+                nip
+        );
+
+        reference.child("user_detail").child(Utility.uidCurrentUser).setValue(modelTeacher).addOnCompleteListener(task -> {
+            if (task.isSuccessful()){
+                binding.progressCircular.setVisibility(View.GONE);
+                binding.progressCircular.setEnabled(true);
+                Utility.toastLS(Profile.this, "Berhasil mengubah data guru");
+            }else {
+                binding.progressCircular.setVisibility(View.GONE);
+                binding.progressCircular.setEnabled(true);
+                Utility.toastLS(Profile.this, task.getException().getMessage());
+            }
+        });
+    }
+
+    private void saveDataParent(String name_child, String nisn){
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+        ModelParent modelParent = new ModelParent(
+                name_child,
+                nisn
+        );
+        reference.child("user_detail").child(Utility.uidCurrentUser).setValue(modelParent).addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
                 if (task.isSuccessful()){
-                    Utility.toastLS(Profile.this, "Berhasil mengubah profile");
+                    binding.progressCircular.setVisibility(View.GONE);
+                    binding.progressCircular.setEnabled(true);
+                    Utility.toastLS(Profile.this, "Berhasil mengubah data orang tua");
                 }else {
+                    binding.progressCircular.setVisibility(View.GONE);
+                    binding.progressCircular.setEnabled(true);
                     Utility.toastLS(Profile.this, task.getException().getMessage());
                 }
             }
@@ -134,11 +233,54 @@ public class Profile extends AppCompatActivity {
                 ModelUser modelUser = task.getResult().getValue(ModelUser.class);
                 binding.emailEditTi.setText(modelUser.getEmail());
                 binding.usernameEditTi.setText(modelUser.getUsername());
+
                 if (!modelUser.getPhone_number().equals("-")){
                     binding.phoneNumberEditTi.setText(modelUser.getPhone_number());
                 }
+
+                if(!modelUser.getAddress().equals("-")){
+                    binding.streetProfileTi.setText(modelUser.getAddress());
+                }
+
             }else {
                 Utility.toastLS(Profile.this, task.getException().getMessage());
+            }
+        });
+    }
+
+    private void setDataTeacher(){
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+        reference.child("user_detail").child(Utility.uidCurrentUser).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (task.isSuccessful()){
+                    ModelTeacher modelTeacher = task.getResult().getValue(ModelTeacher.class);
+                    if (modelTeacher != null){
+                        binding.fullnameTeacherTi.setText(modelTeacher.getFull_name());
+                        binding.nipGuruTi.setText(modelTeacher.getNip());
+                    }
+
+                }else {
+                    Utility.toastLS(Profile.this, task.getException().getMessage());
+                }
+            }
+        });
+    }
+
+    private void setDataChildParent(){
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+        reference.child("user_detail").child(Utility.uidCurrentUser).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (task.isSuccessful()){
+                    ModelParent modelParent = task.getResult().getValue(ModelParent.class);
+                    if (modelParent != null){
+                        binding.nameChildTi.setText(modelParent.getChild_name());
+                        binding.nisnEditTi.setText(modelParent.getNisn());
+                    }
+                }else {
+                    Utility.toastLS(Profile.this, task.getException().getMessage());
+                }
             }
         });
     }
