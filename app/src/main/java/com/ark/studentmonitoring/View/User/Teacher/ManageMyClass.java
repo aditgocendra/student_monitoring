22946438623fc85c577415ac73db90a52ecccacc
aditgocendra <gocendra123@gmail.com>
2022-02-17 23:27:23 +0800

    update

--- app/src/main/java/com/ark/studentmonitoring/View/User/Teacher/ManageMyClass.java
+++ app/src/main/java/com/ark/studentmonitoring/View/User/Teacher/ManageMyClass.java
@@ -7,10 +7,10 @@ import android.widget.Button;
 
 import com.ark.studentmonitoring.R;
 import com.ark.studentmonitoring.Utility;
-import com.ark.studentmonitoring.View.User.Administrator.ManageStudentClass;
+
 import com.ark.studentmonitoring.databinding.ActivityManageMyClassBinding;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
-import com.google.android.material.textfield.TextInputEditText;
+
 
 public class ManageMyClass extends AppCompatActivity {
 
@@ -28,36 +28,27 @@ public class ManageMyClass extends AppCompatActivity {
     }
 
     private void listenerClick() {
-        binding.backBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Utility.updateUI(ManageMyClass.this, DashboardTeacher.class);
-                finish();
-            }
+        binding.backBtn.setOnClickListener(view -> {
+            Utility.updateUI(ManageMyClass.this, DashboardTeacher.class);
+            finish();
         });
 
-        binding.cardEditMyclass.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                bottomSheetDialog = new BottomSheetDialog(ManageMyClass.this);
-                setEditBottomDialog();
-                bottomSheetDialog.show();
-            }
+        binding.cardEditMyclass.setOnClickListener(view -> {
+            bottomSheetDialog = new BottomSheetDialog(ManageMyClass.this);
+            setEditBottomDialog();
+            bottomSheetDialog.show();
         });
 
-        binding.floatAddMyClass.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                bottomSheetDialog = new BottomSheetDialog(ManageMyClass.this);
-                setAddBottomDialog();
-                bottomSheetDialog.show();
-            }
+        binding.floatAddMyClass.setOnClickListener(view -> {
+            bottomSheetDialog = new BottomSheetDialog(ManageMyClass.this);
+            setAddBottomDialog();
+            bottomSheetDialog.show();
         });
     }
 
     private void setAddBottomDialog(){
         View viewBottomDialog = getLayoutInflater().inflate(R.layout.layout_dialog_add_myclass, null, false);
-        TextInputEditText priceWholesaleTiAdd = viewBottomDialog.findViewById(R.id.price_wholesale_ti_add);
+
         Button finishAddBtn = viewBottomDialog.findViewById(R.id.finish_add_btn);
 
         finishAddBtn.setOnClickListener(view -> bottomSheetDialog.dismiss());
@@ -67,7 +58,7 @@ public class ManageMyClass extends AppCompatActivity {
 
     private void setEditBottomDialog(){
         View viewBottomDialog = getLayoutInflater().inflate(R.layout.layout_dialog_edit_myclass, null, false);
-        TextInputEditText priceWholesaleTiAdd = viewBottomDialog.findViewById(R.id.price_wholesale_ti_add);
+
         Button finishEditBtn = viewBottomDialog.findViewById(R.id.finish_edit_btn);
 
         finishEditBtn.setOnClickListener(view -> bottomSheetDialog.dismiss());
