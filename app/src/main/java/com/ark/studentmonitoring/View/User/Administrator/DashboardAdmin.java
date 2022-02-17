22a0feed91525a0b18ad2fc93eb3644ab320c911
aditgocendra <gocendra123@gmail.com>
2022-02-17 23:23:33 +0800

    add intent all card

--- app/src/main/java/com/ark/studentmonitoring/View/User/Administrator/DashboardAdmin.java
+++ app/src/main/java/com/ark/studentmonitoring/View/User/Administrator/DashboardAdmin.java
@@ -2,12 +2,12 @@ package com.ark.studentmonitoring.View.User.Administrator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+
 
 import com.ark.studentmonitoring.Utility;
 import com.ark.studentmonitoring.View.User.Profile;
-import com.ark.studentmonitoring.View.User.Teacher.ManageStudent;
 import com.ark.studentmonitoring.databinding.ActivityDashboardAdminBinding;
 
 
@@ -32,20 +32,44 @@ public class DashboardAdmin extends AppCompatActivity {
             finish();
         });
 
-        binding.cardManageAccount.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Utility.updateUI(DashboardAdmin.this, ManageAccount.class);
-                finish();
-            }
+        binding.cardManageAccount.setOnClickListener(view -> Utility.updateUI(DashboardAdmin.this, ManageAccount.class));
+
+        binding.card1Sd.setOnClickListener(view -> {
+            Intent intent = new Intent(DashboardAdmin.this, ManageStudentClass.class);
+            intent.putExtra("class", "1");
+            startActivity(intent);
         });
 
-        binding.card6Sd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Utility.updateUI(DashboardAdmin.this, ManageStudentClass.class);
-                finish();
-            }
+        binding.card2Sd.setOnClickListener(view -> {
+            Intent intent = new Intent(DashboardAdmin.this, ManageStudentClass.class);
+            intent.putExtra("class", "2");
+            startActivity(intent);
         });
+
+        binding.card3Sd.setOnClickListener(view -> {
+            Intent intent = new Intent(DashboardAdmin.this, ManageStudentClass.class);
+            intent.putExtra("class", "3");
+            startActivity(intent);
+        });
+
+        binding.card4Sd.setOnClickListener(view -> {
+            Intent intent = new Intent(DashboardAdmin.this, ManageStudentClass.class);
+            intent.putExtra("class", "4");
+            startActivity(intent);
+        });
+
+        binding.card5Sd.setOnClickListener(view -> {
+            Intent intent = new Intent(DashboardAdmin.this, ManageStudentClass.class);
+            intent.putExtra("class", "5");
+            startActivity(intent);
+        });
+
+        binding.card6Sd.setOnClickListener(view -> {
+            Intent intent = new Intent(DashboardAdmin.this, ManageStudentClass.class);
+            intent.putExtra("class", "6");
+            startActivity(intent);
+        });
+
+        binding.cardManageYearSchool.setOnClickListener(view -> Utility.updateUI(DashboardAdmin.this, ManageYearSchool.class));
     }
 }
