24da1dafb88fc0b0cae24bc651b17dbd0aef72dc
aditgocendra <gocendra123@gmail.com>
2022-02-17 23:28:16 +0800

    CRUD student

--- app/src/main/java/com/ark/studentmonitoring/View/User/Teacher/ManageStudent.java
+++ app/src/main/java/com/ark/studentmonitoring/View/User/Teacher/ManageStudent.java
@@ -1,22 +1,36 @@
 package com.ark.studentmonitoring.View.User.Teacher;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
-import android.widget.Button;
 
-import com.ark.studentmonitoring.R;
+
+import com.ark.studentmonitoring.Adapter.AdapterManageStudent;
+import com.ark.studentmonitoring.Model.ModelStudent;
+
 import com.ark.studentmonitoring.Utility;
 import com.ark.studentmonitoring.databinding.ActivityManageStudentBinding;
-import com.google.android.material.bottomsheet.BottomSheetDialog;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ManageStudent extends AppCompatActivity {
 
     private ActivityManageStudentBinding binding;
-    private BottomSheetDialog bottomSheetDialog;
+
+    private List<ModelStudent> listStudent;
+    private AdapterManageStudent adapterManageStudent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +40,12 @@ public class ManageStudent extends AppCompatActivity {
         Utility.checkWindowSetFlag(this);
 
         listenerClick();
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        binding.recycleManageStudent.setLayoutManager(layoutManager);
+        binding.recycleManageStudent.setItemAnimator(new DefaultItemAnimator());
+
+        setDataStudent();
     }
 
     private void listenerClick() {
@@ -40,42 +60,27 @@ public class ManageStudent extends AppCompatActivity {
                 Utility.updateUI(ManageStudent.this, AddStudent.class);
             }
         });
-
-        bottomSheetDialog = new BottomSheetDialog(ManageStudent.this);
-        setBottomDialog();
-        binding.cardEditDataStudent.setOnClickListener(new View.OnClickListener() {
+    }
+    private void setDataStudent() {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+        reference.child("student").addValueEventListener(new ValueEventListener() {
             @Override
-            public void onClick(View view) {
-                bottomSheetDialog.show();
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                listStudent = new ArrayList<>();
+                for (DataSnapshot ds : snapshot.getChildren()){
+                    ModelStudent modelStudent = ds.getValue(ModelStudent.class);
+                    modelStudent.setKey(ds.getKey());
+                    listStudent.add(modelStudent);
+                }
+                adapterManageStudent = new AdapterManageStudent(ManageStudent.this, listStudent);
+                binding.recycleManageStudent.setAdapter(adapterManageStudent);
+            }
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Utility.toastLS(ManageStudent.this, error.getMessage());
             }
         });
     }
 
-    private void setBottomDialog(){
-        View viewBottomDialog = getLayoutInflater().inflate(R.layout.layout_dialog_edit_student, null, false);
-        AutoCompleteTextView autoCompleteGender = viewBottomDialog.findViewById(R.id.auto_complete_gender);
-        AutoCompleteTextView autoCompleteDiagnosa = viewBottomDialog.findViewById(R.id.auto_complete_diagnosa);
-        Button finishEditBtn = viewBottomDialog.findViewById(R.id.finish_edit_btn);
-
-        String[] gender = {"Laki-laki","Perempuan"};
-        ArrayAdapter<String> genderAdapter;
-        genderAdapter = new ArrayAdapter<>(this, R.layout.layout_option_item, gender);
-        autoCompleteGender.setAdapter(genderAdapter);
-        autoCompleteGender.setOnItemClickListener((adapterView, view, i, l) -> {
-            String item = adapterView.getItemAtPosition(i).toString();
-        });
-
-        String[] diagnosa = {"Keterbelakangan Mental","Hyperaktif", "Speech Delay"};
-        ArrayAdapter<String> diagnosaAdapter;
-        diagnosaAdapter = new ArrayAdapter<>(this, R.layout.layout_option_item, diagnosa);
-        autoCompleteDiagnosa.setAdapter(diagnosaAdapter);
-        autoCompleteDiagnosa.setOnItemClickListener((adapterView, view, i, l) -> {
-            String item = adapterView.getItemAtPosition(i).toString();
-        });
-
-        finishEditBtn.setOnClickListener(view -> bottomSheetDialog.dismiss());
-
-        bottomSheetDialog.setContentView(viewBottomDialog);
-    }
 }
