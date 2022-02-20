dbd6b552cd189d472452ce8c89619b6efa0e3d11
aditgocendra <gocendra123@gmail.com>
2022-02-20 17:32:57 +0800

    add delete function

--- app/src/main/java/com/ark/studentmonitoring/Adapter/AdapterSeeStudentMyClass.java
+++ app/src/main/java/com/ark/studentmonitoring/Adapter/AdapterSeeStudentMyClass.java
@@ -1,9 +1,13 @@
 package com.ark.studentmonitoring.Adapter;
 
+import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,7 +16,15 @@ import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.ark.studentmonitoring.Model.ModelStudent;
+import com.ark.studentmonitoring.Model.ModelStudentInClass;
+import com.ark.studentmonitoring.Model.ModelValueStudent;
 import com.ark.studentmonitoring.R;
+import com.ark.studentmonitoring.Utility;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.List;
 
@@ -20,6 +32,7 @@ public class AdapterSeeStudentMyClass extends RecyclerView.Adapter<AdapterSeeStu
 
     private Context mContext;
     private List<ModelStudent> listStudentInClass;
+    private DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
 
     public AdapterSeeStudentMyClass(Context mContext, List<ModelStudent> listStudentInClass) {
         this.mContext = mContext;
@@ -31,11 +44,13 @@ public class AdapterSeeStudentMyClass extends RecyclerView.Adapter<AdapterSeeStu
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
         View view = layoutInflater.inflate(R.layout.layout_manage_student, parent, false);
+
         return new MyViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
         ModelStudent modelStudent = listStudentInClass.get(position);
         holder.cardEdit.setVisibility(View.GONE);
 
@@ -45,9 +60,36 @@ public class AdapterSeeStudentMyClass extends RecyclerView.Adapter<AdapterSeeStu
             holder.iconStudent.setImageResource(R.drawable.women_student);
         }
 
-        holder.nameText.setText("Name : "+ modelStudent.getName());
+        holder.nameText.setText("Name : "+ Utility.capitalizeWord(modelStudent.getName()));
         holder.nisnText.setText("NISN : "+modelStudent.getNisn());
 
+        holder.cardDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Create the Dialog here
+                Dialog dialog = new Dialog(mContext);
+                dialog.setContentView(R.layout.custom_dialog_delete);
+                dialog.getWindow().setBackgroundDrawable(mContext.getDrawable(R.drawable.custom_dialog_background));
+
+                dialog.getWindow().setLayout(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT);
+
+                dialog.setCancelable(false); //Optional
+                dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation; //Setting the animations to dialog
+
+                Button Okay = dialog.findViewById(R.id.btn_okay);
+                Button Cancel = dialog.findViewById(R.id.btn_cancel);
+
+                dialog.show();
+                Okay.setOnClickListener(v -> {
+                    deleteStudentInClass(modelStudent.getKey());
+                    dialog.dismiss();
+                });
+
+                Cancel.setOnClickListener(v -> dialog.dismiss());
+            }
+        });
     }
 
     @Override
@@ -68,5 +110,59 @@ public class AdapterSeeStudentMyClass extends RecyclerView.Adapter<AdapterSeeStu
             cardEdit = itemView.findViewById(R.id.card_edit_data_student);
             cardDelete = itemView.findViewById(R.id.card_delete_data_student);
         }
+    }
+
+    private void deleteStudentInClass(String key) {
+
+        reference.child("student_in_class").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot ds : snapshot.getChildren()){
+                    for (DataSnapshot dataSnapshot : ds.getChildren()){
+                        for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()){
+                            ModelStudentInClass modelStudentInClass = dataSnapshot1.getValue(ModelStudentInClass.class);
+                            if (modelStudentInClass.getKey_student().equals(key)){
+                                dataSnapshot1.getRef().removeValue();
+                                removeValueStudent(key);
+                            }
+                        }
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Utility.toastLS(mContext, "Database : "+error);
+            }
+        });
+
+
+    }
+
+    private void removeValueStudent(String key){
+        reference.child("value_student").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot ds : snapshot.getChildren()){
+                    Log.d("test", key+ " / "+ ds.getKey());
+                    if (ds.getKey().equals(key)){
+                        for (DataSnapshot ds1 :ds.getChildren()){
+                            for (DataSnapshot ds2 : ds1.getChildren()){
+                                ModelValueStudent modelValueStudent = ds2.getValue(ModelValueStudent.class);
+
+                                if (modelValueStudent != null){
+                                    ds2.getRef().removeValue();
+                                    ((Activity)mContext).finish();
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
