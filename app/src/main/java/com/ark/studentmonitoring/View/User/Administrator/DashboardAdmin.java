e04b1c9395c5274bc9bcc4f241a0c7f943d643d0
aditgocendra <gocendra123@gmail.com>
2022-02-21 23:34:45 +0800

    set data count

--- app/src/main/java/com/ark/studentmonitoring/View/User/Administrator/DashboardAdmin.java
+++ app/src/main/java/com/ark/studentmonitoring/View/User/Administrator/DashboardAdmin.java
@@ -1,13 +1,23 @@
 package com.ark.studentmonitoring.View.User.Administrator;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 
 
 import com.ark.studentmonitoring.Utility;
 import com.ark.studentmonitoring.databinding.ActivityDashboardAdminBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class DashboardAdmin extends AppCompatActivity {
@@ -22,6 +32,7 @@ public class DashboardAdmin extends AppCompatActivity {
         Utility.checkWindowSetFlag(this);
 
         listenerClick();
+        setLayoutData();
 
     }
 
@@ -69,5 +80,40 @@ public class DashboardAdmin extends AppCompatActivity {
         });
 
         binding.cardManageYearSchool.setOnClickListener(view -> Utility.updateUI(DashboardAdmin.this, ManageYearSchool.class));
+    }
+
+    private long countClass = 0;
+    private void setLayoutData(){
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+        // count student
+        reference.child("student").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (task.isSuccessful()){
+                    binding.textCountStudent.setText("Jumlah Siswa : "+task.getResult().getChildrenCount());
+                }else {
+                    Utility.toastLS(DashboardAdmin.this, task.getException().getMessage());
+                }
+            }
+        });
+
+        // count class
+        reference.child("class").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot ds : snapshot.getChildren()){
+                    countClass += ds.getChildrenCount();
+                    Log.d("test", String.valueOf(ds.getChildrenCount()));
+                }
+                Handler handler = new Handler();
+                handler.postDelayed(() -> binding.textCountClass.setText("Jumlah Kelas : "+String.valueOf(countClass)), 500);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Utility.toastLS(DashboardAdmin.this, "Database : "+error.getMessage());
+            }
+        });
+
     }
 }
