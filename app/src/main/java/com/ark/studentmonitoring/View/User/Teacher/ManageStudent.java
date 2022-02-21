018c50788e1371b8533b56fd823005cab146eec5
aditgocendra <gocendra123@gmail.com>
2022-02-21 17:23:09 +0800

    add load data after scroll on bottom

--- app/src/main/java/com/ark/studentmonitoring/View/User/Teacher/ManageStudent.java
+++ app/src/main/java/com/ark/studentmonitoring/View/User/Teacher/ManageStudent.java
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 
@@ -16,6 +17,8 @@ import com.ark.studentmonitoring.Model.ModelStudent;
 import com.ark.studentmonitoring.Utility;
 import com.ark.studentmonitoring.databinding.ActivityManageStudentBinding;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -24,6 +27,7 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class ManageStudent extends AppCompatActivity {
 
@@ -31,6 +35,11 @@ public class ManageStudent extends AppCompatActivity {
 
     private List<ModelStudent> listStudent;
     private AdapterManageStudent adapterManageStudent;
+    private int maxLoadData = 5;
+    private long countData;
+
+
+    private final DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +54,31 @@ public class ManageStudent extends AppCompatActivity {
         binding.recycleManageStudent.setLayoutManager(layoutManager);
         binding.recycleManageStudent.setItemAnimator(new DefaultItemAnimator());
 
-        setDataStudent();
+        reference.child("student").get().addOnCompleteListener(this::setDataStudent);
+
+        binding.recycleManageStudent.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                if (!binding.recycleManageStudent.canScrollVertically(1) && newState==RecyclerView.SCROLL_STATE_IDLE){
+                    Log.d("Current data", String.valueOf(listStudent.size())+" / "+countData);
+                    if (listStudent.size() < countData){
+                        if (listStudent.size() + maxLoadData <= countData){
+                            int startData = (int) (countData - listStudent.size());
+                            int endData = startData - maxLoadData;
+                            nextLoadData(maxLoadData, startData + 1 , endData);
+                            Log.d("Bottom Scrolled", "Load Next Data");
+                        }else {
+                            int calculateNextLoad = (int) (countData - listStudent.size());
+                            int startData = (int) (countData - listStudent.size());
+                            int endData = startData - calculateNextLoad;
+                            nextLoadData(calculateNextLoad, startData + 1, endData);
+                            Log.d("Calculate next load", String.valueOf(calculateNextLoad));
+                        }
+                    }
+                }
+            }
+        });
     }
 
     private void listenerClick() {
@@ -53,16 +86,11 @@ public class ManageStudent extends AppCompatActivity {
             finish();
         });
 
-        binding.floatAddStudent.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Utility.updateUI(ManageStudent.this, AddStudent.class);
-            }
-        });
+        binding.floatAddStudent.setOnClickListener(view -> Utility.updateUI(ManageStudent.this, AddStudent.class));
     }
-    private void setDataStudent() {
-        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
-        reference.child("student").addValueEventListener(new ValueEventListener() {
+    private void setDataStudent(Task<DataSnapshot> taskCount) {
+        countData = Objects.requireNonNull(taskCount.getResult()).getChildrenCount();
+        reference.child("student").orderByChild("index_student").startAfter(countData - maxLoadData).limitToLast(maxLoadData).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 listStudent = new ArrayList<>();
@@ -74,12 +102,37 @@ public class ManageStudent extends AppCompatActivity {
                 adapterManageStudent = new AdapterManageStudent(ManageStudent.this, listStudent);
                 binding.recycleManageStudent.setAdapter(adapterManageStudent);
             }
-
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
                 Utility.toastLS(ManageStudent.this, error.getMessage());
             }
         });
     }
+
+    private void nextLoadData(int limitNextLoad, int startIndex, int endIndex){
+        reference.child("student").orderByChild("index_student").startAfter(endIndex).endBefore(startIndex).limitToLast(limitNextLoad).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot ds : snapshot.getChildren()){
+
+                    ModelStudent modelStudent = ds.getValue(ModelStudent.class);
+                    modelStudent.setKey(ds.getKey());
+                    listStudent.add(modelStudent);
+                }
+                adapterManageStudent.notifyDataSetChanged();
+
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Utility.toastLS(ManageStudent.this, "Database : "+error.getMessage());
+            }
+        });
+
+
+    }
+
+
+
+
 
 }
